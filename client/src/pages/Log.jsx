11c3a535c43bf91607ae7fdb91af289b2f31f4d0
Joey Sisk <jjmsisk@gmail.com>
2021-03-21 21:25:43 -0500

    rough initial structure

---
+++ client/src/pages/Log.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Log() {
+  return (
+    <main>
+      <h1>Log Page</h1>
+    </main>
+  );
+}
---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Profile() {
+  return (
+    <main>
+      <h1>Profile Page</h1>
+    </main>
+  );
+}
