cf38b4753a341e6f84afc4e8b4aa3046d6084a9f
jeishu <jeremy.zhu@utexas.edu>
2021-03-23 18:57:47 -0500

    made some chanegs for navbar

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./nav.scss"
+import styled from "styled-components";
 
 function Nav() {
   return (
     <header>
       <nav>
-        <NavLink to="/" exact>
-          Home
-        </NavLink>
+        <NavLink to="/" exact>Home</NavLink>
         <NavLink to="/dashboard">Dashboard</NavLink>
         <NavLink to="/log">Log</NavLink>
         <NavLink to="/Profile">About Me</NavLink>
