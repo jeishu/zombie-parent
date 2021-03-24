9231d309b9ad3681af490ce91558a63ce0ebc409
jeishu <jeremy.zhu@utexas.edu>
2021-03-23 21:47:41 -0500

    Mobile design to desktop design for navbar, hover and active styling are in place and router works.

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,20 +1,21 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import "./nav.scss"
-import styled from "styled-components";
-import dashboard from "../../img/nav/dashboard.svg";
-import home from "../../img/nav/home.svg";
-import log from "../../img/nav/log.svg";
-import profile from "../../img/nav/profile.svg";
+// import styled from "styled-components";
+// import dashboard from "../../img/nav/dashboard.svg";
+// import home from "../../img/nav/home.svg";
+// import log from "../../img/nav/log.svg";
+// import profile from "../../img/nav/profile.svg";
 
 function Nav() {
+  const location = useLocation();
   return (
     <header>
       <nav>
-        <NavLink to="/" exact>Home</NavLink>
-        <NavLink to="/dashboard">Dashboard</NavLink>
-        <NavLink to="/log">Log</NavLink>
-        <NavLink to="/Profile">Profile</NavLink>
+        <NavLink to="/" exact className={location.pathname === "/" ? "active" : ""}>Home</NavLink>
+        <NavLink to="/dashboard" className={location.pathname === "/dashboard" ? "active" : ""}>Dashboard</NavLink>
+        <NavLink to="/log" className={location.pathname === "/log" ? "active" : ""}>Log</NavLink>
+        <NavLink to="/profile" className={location.pathname === "/profile" ? "active" : ""}>Profile</NavLink>
       </nav>
     </header>
   );
