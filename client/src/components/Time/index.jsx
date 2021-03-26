ab2455f4a55a82fa64b984aa5c1cac011652c382
jeishu <jeremy.zhu@utexas.edu>
2021-03-25 22:07:57 -0500

    Added a digital clock to the navbar that will only appear in the desktop version

---
+++ client/src/components/Time/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Moment from "react-moment";
+import "./time.scss";
+
+const Time = () => {
+    return (
+        <Moment format="h:mm:ss a" local interval={1} className="time"/>
+    )
+}
+
+export default Time;
