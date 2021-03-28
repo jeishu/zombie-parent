01ad0b68799d760d567eb375d2854a5e7d9ac04d
jeishu <jeremy.zhu@utexas.edu>
2021-03-28 02:35:16 -0500

    added stopwatch component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Stopwatch from "../components/StopWatch/index";
 
 export default function Home() {
   return (
     <main>
       <h1>Home Page</h1>
+      <Stopwatch/>
+
     </main>
   );
 }
