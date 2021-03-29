d3794e8a5e1d76b8d6f2396018d1f026d2e28ba8
Joey Sisk <jjmsisk@gmail.com>
2021-03-29 12:35:11 -0500

    added data Heirarchy display

--- client/src/pages/Log.jsx
+++ client/src/pages/Log.jsx
@@ -1,25 +1,35 @@
 import React, { useState } from "react";
-// import jsonData from "../tempData.json";
+import Hierarchy from "../components/Hierarchy"
 import LogBtn from "../components/LogBtn";
 
 export default function Log() {
   const [btn1, setBtn1] = useState({ name: "Diaper", btn: "Diaper" });
   const [btn2, setBtn2] = useState({ name: "Eat", btn: "Eat" });
   const [btn3, setBtn3] = useState({ name: "Nap", btn: "Nap" });
+  const [dir, setDir] = useState(["Log>"]);
   
-  const updateButtons = (choice) => {
+  const updateButtons = (choice, name) => {
+    addDir(name)
+
     switch (choice) {
       case "Default":
       case "Add Now":
       case "End Time":
       case "Set Time":
         setBtns("Diaper", "Eat", "Nap", "Diaper", "Eat", "Nap");
+        setDir(dir => ["Log>"]);
         break;
       case "Diaper":
         setBtns("Pee", "Poo", "Both", "now/set", "now/set", "now/set");
+        // setDir(dir => [...dir, "Diaper>"]);
         break;
       case "Eat":
         setBtns("Nurse", "Bottle", "Cancel", "Nurse", "Bottle", "Default");
+        // setDir(dir => [...dir, "Eat>"]);
+        break;
+      case "Nap":
+        setBtns("Start Time", "End Time", "Add Time", "Start Time", "End Time", "Add Time");
+        // setDir(dir => [...dir, "Nap>"]);
         break;
       case "Nurse":
         setBtns("Left", "Right", "Cancel", "Feed", "Feed", "Default");
@@ -30,9 +40,6 @@ export default function Log() {
       case "Bottle":
         setBtns("Oz", "Add Now", "Add Time", "Oz", "Add Now", "Add Time");
         break;
-      case "Nap":
-        setBtns("Start Time", "End Time", "Add Time", "Start Time", "End Time", "Add Time");
-        break;
       case "now/set":
         setBtns("Add Now", "Set Time", "Cancel", "Add Now", "Set Time", "Default");
         break;
@@ -41,6 +48,12 @@ export default function Log() {
         break;
     }
   };
+
+  const addDir = (btnName) => {
+    if (btnName != "Switch" && btnName != "Start" && btnName != "Oz" && btnName != "Start Time") {
+      setDir(dir => [...dir, btnName + ">"]);
+    }
+  }
 
   const setBtns = (name1, name2, name3, button1, button2, button3) => {
     setBtn1({ name: name1, btn: button1 });
@@ -55,20 +68,21 @@ export default function Log() {
     <main>
       <h1>Log Page</h1>
       <main>
+        <Hierarchy dir={dir} />
         {/* <LogBtn buttonContent="Back" btnAction={() => handleClick} /> <br /> */}
         <LogBtn
           buttonContent={btn1.name}
-          btnAction={() => updateButtons(btn1.btn)}
+          btnAction={() => updateButtons(btn1.btn, btn1.name)}
         />{" "}
         <br />
         <LogBtn
           buttonContent={btn2.name}
-          btnAction={() => updateButtons(btn2.btn)}
+          btnAction={() => updateButtons(btn2.btn, btn2.name)}
         />{" "}
         <br />
         <LogBtn
           buttonContent={btn3.name}
-          btnAction={() => updateButtons(btn3.btn)}
+          btnAction={() => updateButtons(btn3.btn, btn3.name)}
         />{" "}
         <br />
       </main>
