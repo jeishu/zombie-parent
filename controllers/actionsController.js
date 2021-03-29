e08c1c1759288295feb73a5956d7efb72a18ff1c
Dan Aument <danaument@gmail.com>
2021-03-29 16:26:12 -0500

    moved date range and last 24hrs methods to actions controller

--- controllers/actionsController.js
+++ controllers/actionsController.js
@@ -1,6 +1,36 @@
 const db = require("../models");
 
 module.exports = {
+  findActionsByDateRange: function (req, res) {
+    db.Action
+    .findAll(
+      { 
+        _id: req.params.id,
+        endTime:
+        {
+          $gte: new Date(new Date(req.query.startDate).setHours(00, 00, 00)),
+          $lt: new Date(new Date(req.query.endDate).setHours(23, 59, 59))
+           }
+      },
+      { sort: -1}
+      )
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err));
+  },
+  findActionsLastDay: function (req, res) {
+    db.Action
+    .findAll(
+      {
+        _id: req.params.id,
+        endtime: {
+          $gte: new Date(new Date().getTime() - (24 * 60 * 60 * 1000))
+        }
+      },
+      { sort: -1}
+    )
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err));
+  },
   findById: function(req, res) {
     db.Action
       .findById(req.params.id)
