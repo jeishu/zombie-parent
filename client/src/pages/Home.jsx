28e994300a948b72da3e2448e1e9aab3fb07392c
Darren Garcia <darrengarcia159@gmail.com>
2021-03-31 18:46:22 -0500

    update to dashboard and added font

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,54 @@
 import React from "react";
 import Stopwatch from "../components/StopWatch/index";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Paper } from "@material-ui/core";
+import PieChart from "../components/PieChart/PieChart";
+
+
+
+
+const useStyles = makeStyles((theme) => ({
+  grid: {
+    width: '100%',
+    margin: '0px',
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: '#ffffff',
+    background: '#444444',
+    fontFamily: 'Rubik',
+    fontWeight: '500',
+    fontSize: '1.5rem',
+    letterSpacing: '1.5px',
+    padding: '2rem',
+    
+  }
+}));
 
 export default function Home() {
+  const classes = useStyles();
+
+
   return (
     <main>
-      <h1>Home Page</h1>
+      <h1></h1>
+    <Grid container spacing={6} className={classes.grid}>
+      <Grid item xs={6} md={6} lg={4}>
+        <Paper className={classes.paper}>This is for the timer aspect of the Zombie Parent App.</Paper>
+      </Grid>
+      <Grid item xs={6} md={6} lg={4}>
+        <Paper className={classes.paper}>Number of naps taken today.</Paper>
+      </Grid>
+      <Grid item xs={6} md={6} lg={4}>
+        <Paper className={classes.paper}>Circle Chart showing a percentage of all actions done that day.
+          <PieChart/>
+        </Paper>
+      </Grid>
+      <Grid item xs={6} md={6} lg={12}>
+        <Paper className={classes.paper}>Visual representation of the day.</Paper>
+      </Grid>
+    </Grid>
     </main>
   );
 }
