1993471d866ed7c2cb9138b8d3ce00f3b420857f
Dan Aument <danaument@gmail.com>
2021-03-31 14:18:12 -0500

    first route in utils/api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -14,7 +14,7 @@ export default {
     return axios.delete("/api/books/" + id);
   },
   // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
+  saveAction: function(actionData) {
+    return axios.post("/api/actions", actionData);
   }
 };
