273a14a7802482c11738dc396463de403e6dcad6
Joey Sisk <jjmsisk@gmail.com>
2021-03-31 13:24:25 -0500

    log dir navigatable, needs to clear back to where user is

--- client/src/img/nav/Log.jsx
+++ client/src/img/nav/Log.jsx
@@ -5,7 +5,7 @@ const Log = () => {
   return (
     <svg
       id="Capa_1"
-      enable-background="new 0 0 512 512"
+      enableBackground="new 0 0 512 512"
       height="512"
       viewBox="0 0 512 512"
       width="512"
