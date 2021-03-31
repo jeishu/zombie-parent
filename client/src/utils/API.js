132b3be2a3513df73e07fdb1321f0cbd945bd30a
Dan Aument <danaument@gmail.com>
2021-03-31 17:04:14 -0500

    added existing routes to utils/api, changed /api/childs to children

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,52 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
-  },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
-  },
-  // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
-  },
-  // Saves a book to the database
-  saveAction: function(actionData) {
+  createAction: function(actionData) {
     return axios.post("/api/actions", actionData);
+  },
+  getAction: function(id) {
+    return axios.get("/api/actions/" + id);
+  },
+  updateAction: function(id, actionData) {
+    return axios.put("/api/actions/" + id, actionData);
+  },
+  deleteAction: function(id) {
+    return axios.delete("/api/actions/" + id);
+  },
+  getActionsLastDay: function(childId) {
+    return axios.get("/api/actions/lastday/" + childId);
+  },
+  getActionsLastDayByName: function (childId, name) {
+    return axios.get("/api/actions/lastday/" + childId + "/" + name);
+  },
+  getActionsLastWeek: function(childId) {
+    return axios.get("/api/actions/lastweek/" + childId);
+  },
+  getActionsLastWeekByName: function (childId, name) {
+    return axios.get("/api/actions/lastweek/" + childId + "/" + name);
+  },
+  createUser: function (userData) {
+    return axios.post("/api/users", userData);
+  },
+  getUser: function (id) {
+    return axios.get("/api/users/" + id);
+  },
+  updateUser: function (id, userData) {
+    return axios.put("/api/users/" + id, userData);
+  },
+  deleteUser: function (id) {
+    return axios.delete("/api/users/" + id);
+  },
+  createChild: function (childData) {
+    return axios.post("/api/children/", childData);
+  },
+  getChild: function (id) {
+    return axios.get("/api/children/" + id);
+  },
+  updateChild: function (id, childData) {
+    return axios.put("/api/children/" + id, childData);
+  },
+  deleteChild: function (id) {
+    return axios.delete("/api/children/" + id);
   }
 };
