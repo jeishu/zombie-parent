4220dc21c4304ff59c21f5824dc37e7d56c0b128
jeishu <jeremy.zhu@utexas.edu>
2021-03-31 22:47:06 -0500

    small syntax

--- client/src/components/Hierarchy/index.jsx
+++ client/src/components/Hierarchy/index.jsx
@@ -8,12 +8,8 @@ const Hierarchy = (props) => {
         return (
           <li
             key={dir.key}
-<<<<<<< HEAD
-            onClick={() => props.dirTree(dir, dir.btn, dir.name)}
-=======
             style={{ listStyleType: "none", color: "white", display: "inline" }}
             onClick={() => props.dirTree(dir).then(() => props.updateButtons(dir.btn, dir.name, dir.key - 1))}
->>>>>>> origin
           >
             {dir.name}{`>`}
           </li>
