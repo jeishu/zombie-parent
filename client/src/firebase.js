738e77d00ff2fff7a359a63aa484190480fd6a3a
jeishu <jeremy.zhu@utexas.edu>
2021-03-31 21:14:46 -0500

    fixed a small syntax error err/stack to err.stack

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -13,7 +13,7 @@ try {
     firebase.initializeApp (firebaseConfig);
 } catch (err) {
     if(!/already exists/.test(err.message)) {
-        console.error('Firebase initialization error', err/stack);
+        console.error('Firebase initialization error', err.stack);
     }
 }
 
