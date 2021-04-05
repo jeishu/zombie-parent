d4cfc7894a9d2822d602615d7519fe9f672ad09b
jeishu <jeremy.zhu@utexas.edu>
2021-04-04 21:38:35 -0500

    Added a Zombie Parent Logo temporarily, replaced nav icons, styled the hierarchy a bit more

--- client/src/components/Hierarchy/index.jsx
+++ client/src/components/Hierarchy/index.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import "./hierarchy.scss"
+import "./hierarchy.scss";
+import RightArrow from "../../img/misc/rightArrow";
 
 const Hierarchy = (props) => {
   return (
     <ul className="log-order">
       {props.dir.map((dir) => {
         return (
-          <li
-            key={dir.key}
-            onClick={() => props.dirTree(dir).then(() => props.updateButtons(dir.btn, dir.name, dir.key - 1))}
-          >
-            {dir.name}{` > `}
-          </li>
+          <>
+            <li
+              className="hierarchy"
+              key={dir.key}
+              onClick={() => props.dirTree(dir).then(() => props.updateButtons(dir.btn, dir.name, dir.key - 1))}
+            >
+              <span className="hierarchy-name"> {dir.name} </span>
+                <RightArrow />
+            </li>
+          </>
         );
       })}
     </ul>
