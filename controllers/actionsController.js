4f8cc389b467ab7f88dbe3c1bc8b62c28b761881
Dan Aument <danaument@gmail.com>
2021-04-06 13:53:06 -0500

    changed all update routes to new:true in controllers

--- controllers/actionsController.js
+++ controllers/actionsController.js
@@ -100,7 +100,7 @@ module.exports = {
       .catch((err) => res.status(422).json(err));
   },
   update: function (req, res) {
-    db.Action.findOneAndUpdate({ _id: req.params.id }, req.body)
+    db.Action.findOneAndUpdate({ _id: req.params.id }, req.body, {new: true})
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
--- controllers/childrenController.js
+++ controllers/childrenController.js
@@ -14,7 +14,7 @@ module.exports = {
       .catch((err) => res.status(422).json(err));
   },
   update: function (req, res) {
-    db.Child.findOneAndUpdate({ _id: req.params.id }, req.body)
+    db.Child.findOneAndUpdate({ _id: req.params.id }, req.body, {new: true})
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
