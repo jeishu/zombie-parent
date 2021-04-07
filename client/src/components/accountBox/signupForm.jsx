8b191442ddd76a04ee1941111adf38f7d1d467c8
Darren Garcia <darrengarcia159@gmail.com>
2021-04-06 21:37:27 -0500

    finished style of login

---
+++ client/src/components/accountBox/signupForm.jsx
@@ -0,0 +1,34 @@
+import React, { useContext } from "react";
+import {
+    BoldLink,
+    BoxContainer,
+    FormContainer,
+    Input,
+    MutedLink,
+    SubmitButton,
+} from "./common";
+import { Marginer } from "../marginer";
+import { AccountContext } from "./accountContext";
+
+export function SignupForm(props) {
+    const { switchToSignin } = useContext(AccountContext);
+
+    return (
+        <BoxContainer>
+          <FormContainer>
+            <Input type="text" placeholder="Full Name" />
+            <Input type="email" placeholder="Email" />
+            <Input type="password" placeholder="Password" />
+          </FormContainer>
+          <Marginer direction="vertical" margin={10} />
+          <SubmitButton type="submit">Signup</SubmitButton>
+          <Marginer direction="vertical" margin="1em" />
+          <MutedLink href="#">
+            Already have an account?
+            <BoldLink href="#" onClick={switchToSignin}>
+              Sign in
+            </BoldLink>
+          </MutedLink>
+        </BoxContainer>
+      );
+}
