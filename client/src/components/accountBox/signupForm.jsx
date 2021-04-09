18bd54ba31a65d3e675246a35ec9a327a72a36be
a-h <alexandra.k.hinton1@gmail.com>
2021-04-09 19:46:09 -0400

    fix email signup form

--- client/src/components/accountBox/signupForm.jsx
+++ client/src/components/accountBox/signupForm.jsx
@@ -1,32 +1,62 @@
-import React, { useContext } from "react";
+import Fire from "../../Fire";
+import React, { useState, useContext } from "react";
 import {
-    BoldLink,
-    BoxContainer,
-    FormContainer,
-    Input,
-    MutedLink,
-    SubmitButton,
+  BoldLink,
+  BoxContainer,
+  FormContainer,
+  Input,
+  MutedLink,
+  SubmitButton,
 } from "./common";
 import { Marginer } from "../marginer";
 import { AccountContext } from "./accountContext";
 
-export function SignupForm(props) {
-    const { switchToSignin } = useContext(AccountContext);
 
-    return (
-        <BoxContainer>
-          <FormContainer>
-            <Input type="text" placeholder="Full Name" />
-            <Input type="email" placeholder="Email" />
-            <Input type="password" placeholder="Password" />
-          </FormContainer>
-          <Marginer direction="vertical" margin={10} />
-          <SubmitButton type="submit">Signup</SubmitButton>
-          <Marginer direction="vertical" margin="1em" />
-          <MutedLink href="#">Already have an account?</MutedLink>
-            <BoldLink href="#" onClick={switchToSignin}>
-              Sign in
+export function SignupForm(props) {
+  const { switchToSignin } = useContext(AccountContext);
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const signup = (event) => {
+    event.preventDefault();
+
+    // Error handling for incorrect authentication
+    Fire.auth()
+      .createUserWithEmailAndPassword(email, password)
+      // populate user
+      // .then((userCredential) => {
+      //   initUser(userCredential);
+      // })
+      .catch((error) => {
+        console.error("Incorrect username or password");
+      });
+  };
+
+  return (
+    <BoxContainer>
+      <FormContainer>
+        {/* <Input
+          type="text"
+          placeholder="Full Name"
+        /> */}
+        <Input
+          type="email"
+          onChange={({ target }) => setEmail(target.value)}
+          placeholder="Email"
+        />
+        <Input
+          type="password"
+          onChange={({ target }) => setPassword(target.value)}
+          placeholder="Password"
+        />
+      </FormContainer>
+      <Marginer direction="vertical" margin={10} />
+      <SubmitButton type="submit" onClick={signup}>Signup</SubmitButton>
+      <Marginer direction="vertical" margin="1em" />
+      <MutedLink href="#">Already have an account?</MutedLink>
+      <BoldLink href="#" onClick={switchToSignin}>
+        Sign in
             </BoldLink>
-        </BoxContainer>
-      );
+    </BoxContainer>
+  );
 }
